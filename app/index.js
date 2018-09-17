d2854db37f9437a45a3c3bb9ea5472f6abfaba3e
joshbardsley1202 <jlbardsley@yahoo.com>
2018-09-16 22:20:22 -0600

    You should create an array of numbers in a Javascript file, if there isn't one. Write a function that generates a sum.

--- app/index.js
+++ app/index.js
@@ -12,3 +12,29 @@ var someArray = [
         age:21
     }
 ]
+
+const array = [1,2,3,4,5]
+
+function addArray(array) {
+
+    //With Forloop
+    // var sum;
+    // for(var i = 0; i<array.length; i++){
+    //     sum+=array[i]
+    // }
+
+    //with reduce
+    return array.reduce((sum, num) => {
+        sum+=num
+        return sum
+    })
+
+    //with foreach
+    // var sum;
+    // array.forEach(num => {
+    //     sum+=num
+    // })
+
+    return array.reduce()
+}
+console.log(addArray(array))
